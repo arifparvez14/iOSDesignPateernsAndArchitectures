482e47fb95f0b06b30c6d02021a6b6bbca088c10
Muhammad Ariful Islam <arif.mbstu22@gmail.com>
2023-06-18 23:33:02 +0600

    New pattern added
    New pattern added

--- Behavioral/Template/Template_RealLifeExample.playground/Contents.swift
+++ Behavioral/Template/Template_RealLifeExample.playground/Contents.swift
@@ -1,16 +1,117 @@
 import UIKit
+import AVFoundation
+import CoreLocation
+import Photos
 
-
-
-//MARK: - StrategyRealWorld
-class TemplateRealWorld {
-    func test() {
+class PermissionAccessor: CustomStringConvertible {
+    
+    typealias Completion = (Bool) -> ()
+    
+    func requestAccessIfNeeded(_ completion: @escaping Completion) {
         
+        guard !hasAccess() else { completion(true); return}
+        
+        willReceiveAccess()
+        
+        requestAccess { status in
+            status ? self.didReceiveAccess(): self.didRejectAccess()
+        }
+        
+    }
+    
+    func requestAccess(_ completion: @escaping Completion) {
+        fatalError("Should be overridden")
+    }
+    
+    func hasAccess() -> Bool {
+        fatalError("Should be overridden")
+    }
+    
+    var description: String { return "PermissionAccessor"}
+    
+    // Hooks
+    func willReceiveAccess() {}
+    
+    func didReceiveAccess() {}
+    
+    func didRejectAccess() {}
+}
+
+class CameraAccessor: PermissionAccessor {
+    
+    override func requestAccess(_ completion: @escaping Completion) {
+        AVCaptureDevice.requestAccess(for: .video) { status in
+            return completion(status)
+        }
+    }
+    
+    override func hasAccess() -> Bool {
+        return AVCaptureDevice.authorizationStatus(for: .video) == .authorized
+    }
+    
+    override var description: String { return "Microphone"}
+}
+
+class MicrophoneAccessor: PermissionAccessor {
+    
+    override func requestAccess(_ completion: @escaping Completion) {
+        AVAudioSession.sharedInstance().requestRecordPermission { status in
+            completion(status)
+        }
+    }
+    
+    override func hasAccess() -> Bool {
+        return AVAudioSession.sharedInstance().recordPermission == .granted
+    }
+    
+    override var description: String { return "Microphone" }
+}
+
+class PhotoLibraryAccessor: PermissionAccessor {
+    
+    override func requestAccess(_ completion: @escaping Completion) {
+        PHPhotoLibrary.requestAuthorization { status in
+            completion(status == .authorized)
+        }
+    }
+    
+    override func hasAccess() -> Bool {
+        return PHPhotoLibrary.authorizationStatus() == .authorized
+    }
+    
+    override var description: String { return "PhotoLibrary" }
+    
+    override func didReceiveAccess() {
+        /// We want to track how many people give access to the PhotoLibrary.
+        print("PhotoLibrary Accessor: Receive access. Updating analytics...")
+    }
+    
+    override func didRejectAccess() {
+        /// ... and also we want to track how many people rejected access.
+        print("PhotoLibrary Accessor: Rejected with access. Updating analytics...")
+    }
+}
+
+
+
+//MARK: - TemplateRealWorld
+class TemplateRealWorld {
+    
+    func testTemplateMethodReal() {
+        
+        let accessors = [CameraAccessor(), MicrophoneAccessor(), PhotoLibraryAccessor()]
+        
+        accessors.forEach { item in
+            item.requestAccessIfNeeded { status in
+                let message = status ? "You have access to " : "You don't have access to "
+                print(message + item.description + "\n")
+            }
+        }
     }
 }
 
 var templateRealWorld = TemplateRealWorld()
-templateRealWorld.test()
+templateRealWorld.testTemplateMethodReal()
 
 
 
---
+++ Behavioral/Visitor/Visitor_RealLifeExample.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+//MARK: - TypeScriptRealWorld
+class VisitorRealWorld {
+    func test() {
+        
+    }
+}
+
+var visitorRealWorld = VisitorRealWorld()
+visitorRealWorld.test()
+
+
+
+
+
+
